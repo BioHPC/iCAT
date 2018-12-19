144693250a2ebf2a4541321c415b126dd2c8eb3d
Ahmad Rajeh <ahmadrjeh@gmail.com>
2018-12-18 20:01:06 -0600

    learn from multiple fields; matrix; pred attempt
    Former-commit-id: 056c0c9e323238b6df34d191e808e85cced557eb [formerly a0acaae81a9a54ec7001d1fa2bb5053238a868e4]
    Former-commit-id: 59a71bf803c3444686949bc66890787d660d1924

--- app.R
+++ app.R
@@ -21,17 +21,27 @@ ui <- fluidPage(
                 accept = c("text/tsv", "text/tab-seperated-values", ".tsv")),
       radioButtons("field", "Analyze Clonotypes By:",
                    choices= list("CDR3 Amino Acid Only" = "aminoAcid",
-                                 "TCRV-CDR3-TCRJ" = "b",
+                                 "TCRV-CDR3-TCRJ" = "aminoAcid vGeneName jGeneName",
                                  "Nucleic Acid (DNA)" = "nucleotide")),
-      textInput("pvalue", "Max p-value", value = 0.2),
-      textInput("thresh", "Min Threshold of Public Sequences", value = 3),
+      textInput("pcut", "Max p-value", value = 0.1),
+      textInput("thresh", "Min Threshold of Public Sequences", value = 1),
       tags$hr(),
       
-      actionButton("run", "Run Analysis")
+      actionButton("run", "Train Model"),
+      actionButton("pred", "Predict Independent Sample")
     ),
     
     mainPanel(
-      plotOutput(outputId = "plot")
+      tags$h3("Pre/Post Distributions: "),
+      plotOutput(outputId = "plot"),
+      br(),
+      br(),
+      tags$h3("Classification Matrix: "),
+      tableOutput('table'),
+      br(),
+      br(),
+      tags$h3("Independent Prediction: "),
+      textOutput('result')
     )
     
   )
@@ -44,15 +54,30 @@ server <- function(input, output) {
     naive <- readPre(input$pre$datapath, input$field)
     vaccs <- readPost(input$post$datapath, input$field)
     t1 <- Sys.time()
-    anl <- analyse(naive, vaccs, input$pre$datapath, input$post$datapath, input$field)
+    anl <- analyse(naive, vaccs, input$pre$datapath, input$post$datapath, input$field, input$pcut, input$thresh)
+    
     t2 <- Sys.time()
     print(round(difftime(t2, t1, units = "secs"), digits = 2))
     return(anl)
   })
   
+  preds <- eventReactive(input$pred, {
+    
+    return(pred(both(), readPost(input$indpt$datapath, input$field), input$indpt$datapath, input$field))
+  })
+  
   output$plot <- renderPlot({
     plotHist(both())
+  })
+  
+  output$table <- renderTable({
+    classMat(both())
+  })
+  output$result  <- renderText({
+    preds()
   })
 }
 
 shinyApp(ui, server)
+
+
